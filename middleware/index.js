654afefb7409d9655bdf6ee3707b0d1f391876f9
czirbel <chloe.zirbel@gmail.com>
2018-04-02 15:53:08 -0700

    login and signup routes

---
+++ middleware/index.js
@@ -0,0 +1,25 @@
+/*
+Redirects a user away from certain pages (ex: sign up) if they are already logged in
+ */
+function loggedIn( request, response, next ) {
+    if( request.session && request.session.userId ) {
+        return response.redirect( '/listing/search/' );
+    }
+    return next();
+}
+
+/*
+Controls access to pages that require a user to be logged in
+ */
+function requiresLogIn( request, response, next ) {
+    if( request.session && request.session.userId ) {
+        return next();
+    } else {
+        res.send('You must be logged in to view this page.')
+    }
+}
+
+module.exports = {
+    loggedIn: loggedIn,
+    requiresLogIn: requiresLogIn
+}
