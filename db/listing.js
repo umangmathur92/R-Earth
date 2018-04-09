62b327192a647816ce00bef0966ad379bad1e690
Chloe Zirbel <chloe.zirbel@gmail.com>
2018-04-09 10:07:13 -0700

    Update listing.js

--- db/listing.js
+++ db/listing.js
@@ -5,14 +5,14 @@ const db = require('../db/index');
 function zipSearch( key ) {
 	if (key) {
 		var term = "'" + key + "%'"
-    	return db.any('SELECT * FROM listings WHERE zipcode LIKE ' + term + ' ORDER BY date DESC');
+    	return db.any('SELECT * FROM listings WHERE zipcode LIKE ' + term + ' ORDER BY post_date DESC');
 	} else {
 		return fetchListings();
 	}
 }
 
 function fetchListings() {
-	return db.any('SELECT * FROM listings ORDER BY date DESC');
+	return db.any('SELECT * FROM listings ORDER BY post_date DESC');
 }
 
 module.exports = {
