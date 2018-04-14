97a88835063b14f4f4b5bd226989f8789bbdf583
umangmathur92 <umangmathur10051992@gmail.com>
2018-04-14 10:32:05 -0700

    Added a method to fetch location coordinates from zipcode

--- public/scripts/submit.js
+++ public/scripts/submit.js
@@ -1,16 +1,16 @@
+var geocoder = new google.maps.Geocoder();
 var latitude, longitude, address, zipcode;
 var locationSpinner = document.getElementById('locationSpinner');
 
+/**Asynchronously fetches current location using HTML5's Geolocation API. If successful, calls the 'reverseGeocodeLatLng' method.*/
 function getLocation() {
     if (navigator.geolocation) {
         setVisibility(locationSpinner, true);
         navigator.geolocation.getCurrentPosition(function onLocationFetchSuccess(position) {
-            //Location(Latitude & Longitude only) Fetched Successfully
-            latitude = position.coords.latitude;
-            longitude = position.coords.longitude;
-            geocodeLatLng(latitude, longitude);
+            var currentLatitude = position.coords.latitude;
+            var currentLongitude = position.coords.longitude;
+            reverseGeocodeLatLng(currentLatitude, currentLongitude);
         }, function onLocationFetchFailure(error) {
-            //Failed to fetch Location
             setVisibility(locationSpinner, false);
             console.log(`Location Fetch Error. Code ${error.code}: ${error.message}`);
             var errorMsgDenied = 'Failed to fetch location.\nPlease manually enable geolocation for this website to autofill your address';
@@ -22,9 +22,9 @@ function getLocation() {
     }
 }
 
-function geocodeLatLng(latitude, longitude) {
-    var geocoder = new google.maps.Geocoder;
-    var latlng = {lat: parseFloat(latitude), lng: parseFloat(longitude)};
+/**Asynchronously fetches the complete address and location of the input coordinates using Google Maps' Geocoder API*/
+function reverseGeocodeLatLng(currentLatitude, currentLongitude) {
+    var latlng = {lat: parseFloat(currentLatitude), lng: parseFloat(currentLongitude)};
     geocoder.geocode({'location': latlng}, function(results, status) {
         setVisibility(locationSpinner, false);
         if (status === 'OK') {
@@ -32,24 +32,47 @@ function geocodeLatLng(latitude, longitude) {
                 var addrComponents = getAddressComponents(results);
                 address = addrComponents[0];
                 zipcode = addrComponents[1];
+                latitude = addrComponents[2];
+                longitude = addrComponents[3];
                 var addressInputField = document.getElementById('address');
                 var zipCodeInputField = document.getElementById('zip');
                 addressInputField.value = address;
                 zipCodeInputField.value = zipcode;
             } else {
-                window.alert('No results found');
+                window.alert('No address results found for your location');
             }
         } else {
-            window.alert('Geocoder failed due to: ' + status);
+            window.alert('Address Geocoder failed due to: ' + status);
         }
     });
 }
 
+/**Asynchronously fetches complete address and location of the input Zip code using Google Maps' Geocoder API*/
+function geocodeZip(inputZip) {
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            if (results[0]) {
+                var addrComponents = getAddressComponents(results);
+                latitude = addrComponents[2];
+                longitude = addrComponents[3];
+                //Use the latitude and longitude values from here if user enters address, zip manually !!
+            } else {
+                console.log('No address results found for the input Zip code');
+            }
+        } else {
+            window.alert('Location Geocoder failed due to: ' + status);
+        }
+    });
+  }
+
+/**Parses input JSON array to return a tuple containing address components*/
 function getAddressComponents(locationResultsArr) {
-    var outputAddress, zip;
+    var outputAddress, zip, resultLat, resultLong;
     var firsLocationResult = locationResultsArr[0];
     var addressComponentsArr = firsLocationResult.address_components;
     outputAddress = firsLocationResult.formatted_address;
+    resultLat = firsLocationResult.geometry.location.lat();
+    resultLong = firsLocationResult.geometry.location.lng();
     for(var i in addressComponentsArr) {
         var addressComponent = addressComponentsArr[i];
         var addressComponentType = addressComponent.types[0];
@@ -58,7 +81,7 @@ function getAddressComponents(locationResultsArr) {
             break;
         }
     }
-    return [outputAddress, zip];
+    return [outputAddress, zip, resultLat, resultLong];
 }
 
 function dragover(event){
