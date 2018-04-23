95ed38ada95104c74a4e8402e4ae417de266b949
czirbel <chloe.zirbel@gmail.com>
2018-04-23 16:08:00 -0700

    send display info

--- routes/displaylisting.js
+++ routes/displaylisting.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const listing = require( '../db/listing' );
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('i was callllledd');
   res.render('displaylisting');
+});
+
+router.get('/:listingId', function(req, res, next) {
+  const listingId = req.params.listingId;
+  var currentListing = listing.getListingById(listingId);
+  currentListing.then( data => {
+    res.render('displaylisting', data);
+    //res.send(data);
+  });
 });
 
 module.exports = router;
